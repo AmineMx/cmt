13760474b4014cb3c5a3e29bcf261c4785f0489e
Amine Kaddache <amine@dzworks.com>
2017-11-18 12:38:23 +0100

    add auto load

--- init.php
+++ init.php
@@ -6,11 +6,19 @@
  * Time: 15:14
  *
  **/
-require_once( 'classes/DataBase.php' );
+
+
+spl_autoload_register('autoLoading');
+function autoLoading($className){
+	 require_once ('classes/'. $className.'.php');
+
+	//echo "<pre>",var_dump($className), "</pre>";die;
+}
+/*require_once( 'classes/DataBase.php' );
 
 require_once( 'classes/View.php' );
 require_once( 'classes/Controller.php' );
-require_once( 'classes/Model.php' );
+require_once( 'classes/Model.php' );*/
 $db = new DataBase( 'root', 'root', 'c' );
 $db->connect();
 
